08b1a62ad815531e5341e28ff57ad0c199aeacee
iuxt <iuxt@qq.com>
2026-07-12 14:00:41 +0800

    Deploying to master from @ iuxt/zahuifan@81d9c59d3f6e2b75f812f8cdad8a6ff47bf2ee24 🚀

--- js/utils.js
+++ js/utils.js
@@ -89,20 +89,6 @@
       return dateSuffix.just
     },
 
-    loadComment: (dom, callback) => {
-      if ('IntersectionObserver' in window) {
-        const observerItem = new IntersectionObserver(entries => {
-          if (entries[0].isIntersecting) {
-            callback()
-            observerItem.disconnect()
-          }
-        }, { threshold: [0] })
-        observerItem.observe(dom)
-      } else {
-        callback()
-      }
-    },
-
     scrollToDest: (pos, time = 500) => {
       const currentPos = window.scrollY
       const isNavFixed = document.getElementById('page-header').classList.contains('fixed')
